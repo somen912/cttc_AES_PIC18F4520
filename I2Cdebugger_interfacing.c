935ff9d07477a2987ea80eb08a91aa45bbb58f65
Somen Das <71995297+somen912@users.noreply.github.com>
2021-04-05 08:06:47 +0530

    Add files via upload

---
+++ I2Cdebugger_interfacing.c
@@ -0,0 +1,142 @@
+
+// PIC18F4520 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1H
+#pragma config OSC = HS         // Oscillator Selection bits (HS oscillator)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 3         // Brown Out Reset Voltage bits (Minimum setting)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = OFF      // MCLR Pin Enable bit (RE3 input pin enabled; MCLR disabled)
+
+// CONFIG4L
+#pragma config STVREN = OFF     // Stack Full/Underflow Reset Enable bit (Stack full/underflow will not cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+
+#define SDA RC4
+#define SCK RC3
+#define I2C_SPEED 100
+
+void init_I2C()
+{
+    TRISC3=1;
+    TRISC4=1;
+    SSPADD = ((_XTAL_FREQ/4000)/I2C_SPEED)-1;
+    SSPSTAT = 0x80;
+    SSPCON1 = 0x28;
+}
+
+void I2C_start()
+{
+    SSPCON2bits.SEN=1;   // start condition enable
+    while(!SSPIF);
+    SSPIF=0;
+}
+
+void I2C_restart()
+{
+    SSPCON2bits.RSEN=1;  // repeat start condition
+    while(!SSPIF);
+    SSPIF=0;
+}
+
+void I2C_stop()
+{
+    SSPCON2bits.PEN=1;  // stop condition enable bit
+    while(!SSPIF);
+    SSPIF=0;
+}
+
+bit I2C_write_byte(unsigned char byte)   
+{
+    /* after writing 0xA0 it will return NACK because
+     *  currently no slave is connected to the I2C bus */
+    SSPBUF = byte;
+    while(!SSPIF);
+    SSPIF=0;
+    return ACKSTAT;
+}
+
+unsigned char I2C_read_byte()
+{
+    /* after reading it will return ACK because
+     *  currently no slave is connected to the I2C bus */
+    SSPCON2bits.RCEN=1;  // receive enable bit
+    while(!SSPIF);
+    SSPIF=0;
+    return SSPBUF;
+}
+
+void I2C_send_ACK()
+{ 
+    ACKEN=1;  // acknowledge sequence enable bit
+    while(!SSPIF);
+    SSPIF=0;
+}
+
+void main()
+{
+    unsigned char rx_byte;
+    
+    init_I2C();
+    I2C_start();
+    I2C_write_byte(0x0b);
+    rx_byte= I2C_read_byte();
+    I2C_send_ACK();
+    I2C_stop();
+    
+    while(1)
+    {}
+  
+}
